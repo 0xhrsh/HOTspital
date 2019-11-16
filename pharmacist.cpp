cc7ef75416f1c465a268e0465fbc0d18b516ff78
YashviRamanuj <49070630+YashviRamanuj@users.noreply.github.com>
2019-11-17 02:21:24 +0530

    pharmacist.cpp is updated

--- pharmacist.cpp
+++ pharmacist.cpp
@@ -1,1 +1,37 @@
 #include <bits/stdc++.h>
+
+using namespace std;
+
+class pharmacist
+{
+
+};
+void pharmacistInit(int num)
+{
+	cout << "this is pharmacist" << endl;
+}
+int main()
+{
+	ifstream fin;
+	string pat;
+	cout << "enter the patient's ID:";
+	cin >> pat;
+	fin.open("admin/records.txt");
+	string str;
+	do
+	{
+		
+		getline(fin,str);
+		if(str[0]==pat[0] && str[1]==pat[1] && str[2]==pat[2])  // !!! patient's id is assumed to be in the first column of record
+		{
+			// patient's prescription must be at the end and in the format: "pres: ......"	
+			size_t pos=str.find("pres:");
+			string pres=str.substr(pos);
+			break;
+
+		}
+	}while(getline(fin,str));
+
+
+
+}

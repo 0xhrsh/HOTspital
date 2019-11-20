7dda629724e79e4af1ee2e8bf0d249691dfcee0a
YashviRamanuj <49070630+YashviRamanuj@users.noreply.github.com>
2019-11-20 14:18:26 +0530

    taken prescription from records

--- pharmacist.cpp
+++ pharmacist.cpp
@@ -4,6 +4,39 @@ using namespace std;
 
 class pharmacist
 {
+	string pres(string pat)
+	{	
+		ifstream fin;
+		fin.open("admin/records.txt");
+		string str,pre;
+		do
+		{
+			
+			// getline(fin,str);
+			if(str[0]==pat[0] && str[1]==pat[1] && str[2]==pat[2])  // !!! patient's id is assumed to be in the first column of record
+			{
+				// patient's prescription must be at the end and in the format: "pres: ......"	
+				size_t pos=str.find("pres:");
+				pre=str.substr(pos);
+				break;
+
+			}
+		}while(getline(fin,str));
+		return pre;
+
+	}
+	public:
+	string getPatient()
+	{
+		string pat;
+		cout << "enter the patient's ID:";
+		cin >> pat;	
+		return pat;
+	}
+	string getPrescription(string pat)
+	{
+		return pres(pat);
+	}
 
 };
 void pharmacistInit(int num)
@@ -12,26 +45,9 @@ void pharmacistInit(int num)
 }
 int main()
 {
-	ifstream fin;
-	string pat;
-	cout << "enter the patient's ID:";
-	cin >> pat;
-	fin.open("admin/records.txt");
-	string str;
-	do
-	{
-		
-		getline(fin,str);
-		if(str[0]==pat[0] && str[1]==pat[1] && str[2]==pat[2])  // !!! patient's id is assumed to be in the first column of record
-		{
-			// patient's prescription must be at the end and in the format: "pres: ......"	
-			size_t pos=str.find("pres:");
-			string pres=str.substr(pos);
-			break;
-
-		}
-	}while(getline(fin,str));
-
-
+	pharmacist ph;
+	string patient=ph.getPatient();
+	string pres=ph.getPrescription(patient);
+	
 
 }

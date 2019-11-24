d7dc4fe21c2875288f6debbb2b09b08bb4692083
YashviRamanuj <49070630+YashviRamanuj@users.noreply.github.com>
2019-11-24 05:32:36 +0530

    added updateInventory in pharmacist and modified find prescription

--- pharmacist.cpp
+++ pharmacist.cpp
@@ -1,42 +1,74 @@
 #include <bits/stdc++.h>
-#include "Pclass.cpp"
-using namespace std;
+#include "admin/classes/Fclass.cpp"
+#include"patient.cpp"
+#include"admin/mkrecords/mkInventory.cpp"
+// using namespace std;
 
-class pharmacist{
-	int prescription[20][2];
-	string findPrescription(int LDAP){
-	patient* p=new patient();
-	ifstream fin;
-	fin.open("records/records.txt", ios::in);
-	for (int i = 0; i < 20; ++i){
-		fin.read(reinterpret_cast<char*>(p), sizeof(patient));
-		if(p->LDAP==LDAP){
-			for(int i=0;i<20;i++){
-				prescription[i][0]=p->medicine[i][0];
-				prescription[i][1]=p->medicine[i][1];
-			}
-
+patient* findPrescription(int LDAP)
+	{
+		int prescription[20][2];
+		patient* p=new patient();   // !!!!
+		ifstream fin;
+		fin.open("admin/records/records.txt", ios::in);
+		for (int i = 0; i < 20; ++i)
+		{
+			fin.read(reinterpret_cast<char*>(p), sizeof(patient));
+			if(p->LDAP==LDAP)
+				return p;
 		}
-	}	
+		//return prescription;	
 	
 	}
-public:
-	string getPatient()
+void maintainInventory(patient* p)
 	{
-		string pat;
-		cout << "enter the patient's ID:";
-		cin >> pat;	
-		return pat;
+		ifstream fin;
+		inventory* in;
+		fin.open("admin/records/inventory.txt", ios::in);
+		int i;
+		while(1)
+		{
+			fin.read(reinterpret_cast<char*>(in), sizeof(inventory));
+			if(p->medicine[i][0]!=0 && in->medicineId==p->medicine[i][0])
+			{
+				in->Quantity--;
+				i++;
+			}
+			if(p->medicine[i][0]==0) break;
+		}
+
 	}
-	string getPrescription(string pat)
-	{
-		return findPrescription(pat);
-	}
+
+
+
+class pharmacist
+{
+	friend patient* findPrescription(int LDAP);
+	
+	friend void maintainInventory(patient* p);
+
+	public:
+		int getPatient()
+		{
+			int pat;  //ldap of the patient
+			cout << "enter the patient's ID:";
+			cin >> pat;	
+			return pat;
+		}
+		patient* getPrescription(int pat)
+		{
+
+			return findPrescription(pat);
+		}
+		void updateInventory(patient* p)
+		{
+			maintainInventory(p);
+		}
 
 };
 void pharmacistInit(int num, pharmacist* f)
 {
-	int patient=f->getPatient();
-	string pres=f->getPrescription(patient);
+	int pat=f->getPatient();
+	patient* p=f->getPrescription(pat);
+	f->updateInventory(p);
 
 }
